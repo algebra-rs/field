4089f9194e8f8aa70001d4ef5e7d0551ea0e8d97
nougzarm <nougzarm@icloud.com>
2025-09-17 21:02:40 +0200

    tests

---
+++ tests/big_int.rs
@@ -0,0 +1,21 @@
+use num_bigint::BigInt;
+use field::ring::Ring;
+
+#[test]
+fn test_ring() {
+    let zero = BigInt::from(0);
+    assert!(zero.is_zero()); 
+}
+
+#[test]
+fn test_zero_and_one() {
+    assert_eq!(<BigInt as Ring>::zero(), BigInt::from(0));
+    assert_eq!(<BigInt as Ring>::one(), BigInt::from(1));
+}
+
+#[test]
+fn test_addition() {
+    let a = BigInt::from(2);
+    let b = BigInt::from(3);
+    assert_eq!(a + b, BigInt::from(5));
+}
