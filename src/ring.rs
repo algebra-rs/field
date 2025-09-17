96f83d1c8355074c79afc79a21b8b58a97b5ede4
nougzarm <nougzarm@icloud.com>
2025-09-17 21:34:03 +0200

    from Traits

--- src/ring.rs
+++ src/ring.rs
@@ -9,11 +9,19 @@ pub trait Ring:
     + Sub<Output = Self>
     + Mul<Output = Self>
     + Neg<Output = Self>
+    + From<u128>
+    + From<u64>
+    + From<u32>
+    + From<u16>
+    + From<u8>
+    + From<i128>
+    + From<i64>
+    + From<i32>
+    + From<i16>
+    + From<i8>
 {
     fn zero() -> Self;
     fn one() -> Self;
-
-    fn from_i64(n: i64) -> Self;
 
     fn is_zero(&self) -> bool {
         self == &Self::zero()
@@ -23,6 +31,4 @@ pub trait Ring:
 impl Ring for BigInt {
     fn zero() -> Self { BigInt::from(0) }
     fn one() -> Self { BigInt::from(1) }
-
-    fn from_i64(n: i64) -> Self { BigInt::from(n) }
 }
